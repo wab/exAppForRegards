8fe3ff9d30b4dfcaa89512c01c2daf36c25192ce
wab <contact@wabdesign.fr>
2016-06-16 10:31:06 +0200

    menu bar swipe panel

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -12,9 +12,13 @@
   ])
     .config(config)
     .run(run)
+    .controller('homeCtrl', homeCtrl )
+    .controller('pageCtrl', pageCtrl )
   ;
 
   config.$inject = ['$urlRouterProvider', '$locationProvider'];
+  pageCtrl.$inject = ['$scope', '$rootScope'];
+  homeCtrl.$inject = ['$scope', '$rootScope'];
 
   function config($urlProvider, $locationProvider) {
     $urlProvider.otherwise('/');
@@ -29,6 +33,13 @@
 
   function run() {
     FastClick.attach(document.body);
+  }
+
+  function homeCtrl($scope, $rootScope) {
+    $rootScope.isPage = false;
+  }
+  function pageCtrl($scope, $rootScope) {
+    $rootScope.isPage = true;
   }
 
 })();
